3b1e9e968662229fed752a4e305eab70b8cd56aa
ThePangel <74447121+ThePangel@users.noreply.github.com>
2025-07-22 15:17:07 +0200

    Basic shooting

---
+++ prompt.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+const bulletScene = preload("res://playerBullet.tscn")
+
+const SPEED = 400.0
+const ROTSPEED = 5.0
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("spacebar"):
+		shoot()
+	var direction = Input.get_vector(
+		'ui_left', 'ui_right', 'ui_up', 'ui_down')
+	var rotationAxis = Input.get_axis("A", "D")
+	
+	if rotationAxis != 0:
+		rotation += ROTSPEED * rotationAxis * _delta
+	if direction.length():
+		velocity = direction * SPEED
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, 20)
+		velocity.y = move_toward(velocity.y, 0, 20)
+
+	move_and_slide()
+	position.x = clamp(position.x, 8, 632)
+	position.y = clamp(position.y, 40, 472)
+
+func shoot():
+	var bullet = bulletScene.instantiate() 
+	bullet.global_position = $Marker2D.global_position  
+	bullet.rotation = $Marker2D.global_rotation
+	get_tree().get_current_scene().add_child(bullet)  
